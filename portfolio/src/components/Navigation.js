91cb90efed4af17d17a1e9cf4a2223c39ee050f9
Clarice Tran <tran.clarice@gmail.com>
2023-03-05 20:15:43 -0800

    Updated for pushing to github. Changed links so they worked with /react-portfolio/

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -4,19 +4,19 @@ import { NavLink } from "react-router-dom";
 export default function Navigation() {
     return (
         <nav className="animate__animated animate__fadeIn">
-            <NavLink to="/" exact="true" className={(navData) => (navData.isActive ? "navbar-link active" : "nav-link")}>
+            <NavLink to="/react-portfolio/" exact="true" className={(navData) => (navData.isActive ? "navbar-link active" : "nav-link")}>
                 ABOUT ME
             </NavLink>
 
-            <NavLink to="/portfolio" className={(navData) => (navData.isActive ? "navbar-link active" : "nav-link")}>
+            <NavLink to="/react-portfolio/portfolio" className={(navData) => (navData.isActive ? "navbar-link active" : "nav-link")}>
                 PORTFOLIO
             </NavLink>
 
-            <NavLink to="/contact" className={(navData) => (navData.isActive ? "navbar-link active" : "nav-link")}>
+            <NavLink to="/react-portfolio//contact" className={(navData) => (navData.isActive ? "navbar-link active" : "nav-link")}>
                 CONTACT
             </NavLink>
 
-            <NavLink to="/resume" className={(navData) => (navData.isActive ? "navbar-link active" : "nav-link")}>
+            <NavLink to="/react-portfolio/resume" className={(navData) => (navData.isActive ? "navbar-link active" : "nav-link")}>
                 RESUME
             </NavLink>
         </nav>
